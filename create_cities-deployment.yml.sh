6f618360768f0f4ca4919d02f33601b433d49e53
Calvin Leung Huang <cleung2010@gmail.com>
2016-11-07 16:55:05 -0500

    Initial commit

---
+++ create_cities-deployment.yml.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat > cities-deployment.yml <<EOF
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: cities
+spec:
+  replicas: 3
+  template:
+    metadata:
+      labels:
+        name: cities
+        deployment: ${WERCKER_GIT_COMMIT}
+    spec:
+      containers:
+      - name: cities
+        image: ${AWS_REGISTRY_ID}.dkr.ecr.us-east-1.amazonaws.com/cities:${WERCKER_GIT_COMMIT}
+        ports:
+        - name: http-server
+          containerPort: 8081
+          protocol: TCP
+EOF
